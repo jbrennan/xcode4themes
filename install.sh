f158511133be71705bdcc73e15c738a6ace68444
Stephen <tasb00429@gmail.com>
2016-12-22 11:50:38 +0800

    feat(compile): auto close and reopen xcode when execute ./install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 
 mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 cp *.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+
+kill 15 `lsof -c Xcode`
+open -a Xcode
